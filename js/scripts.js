6910c1975695f3793bd187dba1dee0e930cfe953
apple <apple@Celestes-IMac.local>
2019-12-08 10:58:35 -0800

    add javascript and css styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
-console.log("Hello world!");
+$(document).ready(function () {
+    $("#questionsform").submit(function () {
+        var company = parseInt($('input[name = "company"]:checked').val());
+        var language = parseInt($('input[name = "language"]:checked').val());
+        var school = $("#school").val();
+        var color = $("#color").val();
+        var experience = parseInt($('input[name="experience"]').val());
+        var otherText = parseInt($('input[id="otherValue"]').val());
+        otherText.style.visibility = 'hidden';
+
+        
+
+
+
+
+
+
+
+
+
+
+var result = company +  language + school + color + expereince;
+
+        if (result <= 6) {
+            $("#result").text("Ruby");
+        } else if (result <= 11 ) {
+            $("#result").text("Python");
+        } else if (result <= 16) {
+            $("#result").text("Javascript");
+        }
+        
+
+        event.preventDefault();
+    });
+});
