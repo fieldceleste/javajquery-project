f004af1de8bc43a92b0a203b1824480ae9c7b1e6
apple <apple@Celestes-IMac.local>
2019-12-09 18:15:48 -0800

    add indention to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-    $("#questionsform").submit(function() {
-        var nameInput =$("input#name").val();
+    $("#questionsform").submit(function () {
+        var nameInput = $("input#name").val();
         var company = parseInt($('input[name = "company"]:checked').val());
         var language = parseInt($('input[name = "language"]:checked').val());
         var school = parseInt($("#school").val());
         var color = parseInt($("#color").val());
         var experience = parseInt($("#experience").val());
-        console.log(experience)
         
 
-var result = company +  language + school + color + experience;
+
+        var result = company + language + school + color + experience;
 
         $(".name").text(nameInput);
         if (result <= 5) {
-        $("#answer").text("Ruby, so you can build web applicaions, desktop games, and mobile apps!");
-         } else if (result <= 11 ) {
-        $("#answer").text("Python, so you can do data analysis, software application, and build beautiful websites!");
-          } else if (result <= 16) {
-        $("#answer").text("Javascript, so you can learn how to make mobile apps for Android!");
+            $("#answer").text("Ruby, so you can build web applicaions, desktop games, and mobile apps!");
+        } else if (result <= 11) {
+            $("#answer").text("Python, so you can do data analysis, software application, and build beautiful websites!");
+        } else if (result <= 16) {
+            $("#answer").text("Javascript, so you can learn how to make mobile apps for Android!");
         }
-event.preventDefault();
+        event.preventDefault();
     });
 });
